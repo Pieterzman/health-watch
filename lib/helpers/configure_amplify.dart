cbbfa89fb54dc72bd8335728601f651976a0f22a
Pieterzman <Pieterzman@gmail.com>
2022-05-12 11:56:12 +0200

    Added amplify S3 Storage plugin.

--- lib/helpers/configure_amplify.dart
+++ lib/helpers/configure_amplify.dart
@@ -3,6 +3,7 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 // import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 
 import '../amplifyconfiguration.dart';
 import '../models/ModelProvider.dart';
@@ -11,10 +12,12 @@ Future<void> configureAmplify() async {
   // AmplifyAnalyticsPinpoint analyticsPlugin = AmplifyAnalyticsPinpoint();
   AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
   AmplifyAPI apiPlugin = AmplifyAPI();
+  AmplifyStorageS3 storageS3Plugin = AmplifyStorageS3();
   AmplifyDataStore datastorePlugin =
       AmplifyDataStore(modelProvider: ModelProvider.instance);
 
-  await Amplify.addPlugins([authPlugin, datastorePlugin, apiPlugin]);
+  await Amplify.addPlugins(
+      [authPlugin, datastorePlugin, apiPlugin, storageS3Plugin]);
 
   try {
     ///Wait for plugins to add before configure is called
