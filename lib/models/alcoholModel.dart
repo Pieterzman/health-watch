2358a5fd742be8ff29164dc5d3c1e58e251715ce
Pieterzman <Pieterzman@gmail.com>
2022-05-12 11:54:31 +0200

    Added Alcohol Model to log beverages.

---
+++ lib/models/alcoholModel.dart
@@ -0,0 +1,13 @@
+class AlcoholModel {
+  String id;
+  String type;
+  String description;
+  double volume_ml;
+  double precent;
+  double units;
+  int bevCounter;
+  String assetImage;
+
+  AlcoholModel(this.id, this.type, this.description, this.volume_ml,
+      this.precent, this.units, this.bevCounter, this.assetImage);
+}
