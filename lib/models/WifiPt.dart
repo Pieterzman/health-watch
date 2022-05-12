92e033971890644e8537f8141a01569ab670c797
Pieterzman <Pieterzman@gmail.com>
2022-05-12 11:52:00 +0200

    Added wifi model from amplify datastore

---
+++ lib/models/WifiPt.dart
@@ -0,0 +1,199 @@
+/*
+* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
+
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/foundation.dart';
+
+
+/** This is an auto generated class representing the WifiPt type in your schema. */
+@immutable
+class WifiPt extends Model {
+  static const classType = const _WifiPtModelType();
+  final String id;
+  final String? _healthdatauserID;
+  final String? _loggedWifiIP;
+  final TemporalDateTime? _loggedDate;
+  final TemporalDateTime? _createdAt;
+  final TemporalDateTime? _updatedAt;
+
+  @override
+  getInstanceType() => classType;
+  
+  @override
+  String getId() {
+    return id;
+  }
+  
+  String get healthdatauserID {
+    try {
+      return _healthdatauserID!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  String? get loggedWifiIP {
+    return _loggedWifiIP;
+  }
+  
+  TemporalDateTime? get loggedDate {
+    return _loggedDate;
+  }
+  
+  TemporalDateTime? get createdAt {
+    return _createdAt;
+  }
+  
+  TemporalDateTime? get updatedAt {
+    return _updatedAt;
+  }
+  
+  const WifiPt._internal({required this.id, required healthdatauserID, loggedWifiIP, loggedDate, createdAt, updatedAt}): _healthdatauserID = healthdatauserID, _loggedWifiIP = loggedWifiIP, _loggedDate = loggedDate, _createdAt = createdAt, _updatedAt = updatedAt;
+  
+  factory WifiPt({String? id, required String healthdatauserID, String? loggedWifiIP, TemporalDateTime? loggedDate}) {
+    return WifiPt._internal(
+      id: id == null ? UUID.getUUID() : id,
+      healthdatauserID: healthdatauserID,
+      loggedWifiIP: loggedWifiIP,
+      loggedDate: loggedDate);
+  }
+  
+  bool equals(Object other) {
+    return this == other;
+  }
+  
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is WifiPt &&
+      id == other.id &&
+      _healthdatauserID == other._healthdatauserID &&
+      _loggedWifiIP == other._loggedWifiIP &&
+      _loggedDate == other._loggedDate;
+  }
+  
+  @override
+  int get hashCode => toString().hashCode;
+  
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+    
+    buffer.write("WifiPt {");
+    buffer.write("id=" + "$id" + ", ");
+    buffer.write("healthdatauserID=" + "$_healthdatauserID" + ", ");
+    buffer.write("loggedWifiIP=" + "$_loggedWifiIP" + ", ");
+    buffer.write("loggedDate=" + (_loggedDate != null ? _loggedDate!.format() : "null") + ", ");
+    buffer.write("createdAt=" + (_createdAt != null ? _createdAt!.format() : "null") + ", ");
+    buffer.write("updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
+    buffer.write("}");
+    
+    return buffer.toString();
+  }
+  
+  WifiPt copyWith({String? id, String? healthdatauserID, String? loggedWifiIP, TemporalDateTime? loggedDate}) {
+    return WifiPt._internal(
+      id: id ?? this.id,
+      healthdatauserID: healthdatauserID ?? this.healthdatauserID,
+      loggedWifiIP: loggedWifiIP ?? this.loggedWifiIP,
+      loggedDate: loggedDate ?? this.loggedDate);
+  }
+  
+  WifiPt.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _healthdatauserID = json['healthdatauserID'],
+      _loggedWifiIP = json['loggedWifiIP'],
+      _loggedDate = json['loggedDate'] != null ? TemporalDateTime.fromString(json['loggedDate']) : null,
+      _createdAt = json['createdAt'] != null ? TemporalDateTime.fromString(json['createdAt']) : null,
+      _updatedAt = json['updatedAt'] != null ? TemporalDateTime.fromString(json['updatedAt']) : null;
+  
+  Map<String, dynamic> toJson() => {
+    'id': id, 'healthdatauserID': _healthdatauserID, 'loggedWifiIP': _loggedWifiIP, 'loggedDate': _loggedDate?.format(), 'createdAt': _createdAt?.format(), 'updatedAt': _updatedAt?.format()
+  };
+
+  static final QueryField ID = QueryField(fieldName: "wifiPt.id");
+  static final QueryField HEALTHDATAUSERID = QueryField(fieldName: "healthdatauserID");
+  static final QueryField LOGGEDWIFIIP = QueryField(fieldName: "loggedWifiIP");
+  static final QueryField LOGGEDDATE = QueryField(fieldName: "loggedDate");
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "WifiPt";
+    modelSchemaDefinition.pluralName = "WifiPts";
+    
+    modelSchemaDefinition.authRules = [
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
+    ];
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: WifiPt.HEALTHDATAUSERID,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: WifiPt.LOGGEDWIFIIP,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: WifiPt.LOGGEDDATE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'createdAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'updatedAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+  });
+}
+
+class _WifiPtModelType extends ModelType<WifiPt> {
+  const _WifiPtModelType();
+  
+  @override
+  WifiPt fromJson(Map<String, dynamic> jsonData) {
+    return WifiPt.fromJson(jsonData);
+  }
+}
